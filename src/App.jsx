c8d6f33d9a4b3b244c8c3e7da9db2ab57838f83a
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-09-08 11:50:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ console.log(err)
     </form>
 
 { reference && <div>
-{Object.keys(success).length > 0 && <div> {JSON.stringify(success)} </div>}
+{Object.keys(success).length > 0 && <div> {JSON.stringify(success, null, 2)} </div>}
 <button className="button" type="submit" onClick={checkSuccess}>transfer status</button>  
 </div>}
     </>
